3aceb6fe6d325d6c3f49de554cf56487e5fdc80a
mayeut <mayeut@users.noreply.github.com>
2016-10-08 00:19:17 +0200

    Update travis

--- build.sh
+++ build.sh
@@ -5,16 +5,34 @@ DOCKER_REPO=ljtbuilder2
 
 TAGS="amd64-centos5 arm64-xenial armhf-xenial ppc64el-xenial"
 
-for TAG in ${TAGS}; do
-  docker build -t ${DOCKER_USER}/${DOCKER_REPO}:${TAG} ${TAG}
-done
-
-if [ "${DOCKER_DO_PUSH}" == "1" ] &&  "${DOCKER_DO_LOGIN}" == "1" ; then
-  docker login -u="${DOCKER_USER}" -p="${DOCKER_PASSWORD}"
+if [ "${GENERATE_TRAVIS}" == "1" ]; then
+	echo "matrix:" > .travis.yml
 fi
 
-if [ "${DOCKER_DO_PUSH}" == "1" ]; then
-  for TAG in ${TAGS}; do
-    docker push ${DOCKER_USER}/${DOCKER_REPO}:${TAG}
-  done
+for TAG in ${TAGS}; do
+	if [ "${GENERATE_TRAVIS}" == "1" ]; then
+		echo "  include:" >> .travis.yml
+		echo "    - os: linux" >> .travis.yml
+		echo "      env: DOCKER_TAG=${TAG}" >> .travis.yml
+		echo "      sudo: required" >> .travis.yml
+		echo "      services:" >> .travis.yml
+		echo "        - docker" >> .travis.yml
+	else
+  	docker build -t ${DOCKER_USER}/${DOCKER_REPO}:${TAG} ${TAG}
+  	if [ "${DOCKER_DO_PUSH}" == "1" ]; then
+  		docker push ${DOCKER_USER}/${DOCKER_REPO}:${TAG}
+  	fi
+  fi
+done
+
+if [ "${GENERATE_TRAVIS}" == "1" ]; then
+	echo "before_install:" >> .travis.yml
+	echo "  - docker run --rm --privileged multiarch/qemu-user-static:register" >> .travis.yml
+  echo "script:" >> .travis.yml
+	echo "  - docker build -t ${DOCKER_USER}/${DOCKER_REPO}:\${DOCKER_TAG} \${DOCKER_TAG}" >> .travis.yml
+	echo "after_success:" >> .travis.yml
+  echo "  - if [ \"\${TRAVIS_BRANCH}\" == \"master\" ] && [ \"\${DOCKER_PUSH}\" == \"1\" ]; then" >> .travis.yml
+  echo "    docker login -u=\"${DOCKER_USER}\" -p=\"\${DOCKER_PASSWORD}\";" >> .travis.yml
+  echo "    docker push ${DOCKER_USER}/${DOCKER_REPO}:\${DOCKER_TAG};" >> .travis.yml
+  echo "    fi" >> .travis.yml
 fi
