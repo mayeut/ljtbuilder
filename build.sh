50eb4d106c29f93f53613430d407ea0a54ddf2a0
mayeut <mayeut@users.noreply.github.com>
2016-10-07 23:43:22 +0200

    Refactor build.sh and add travis configuration

--- build.sh
+++ build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash -xe
 
-docker build -t mayeut/ljtbuilder2:arm64-xenial arm64-xenial
-docker build -t mayeut/ljtbuilder2:armhf-xenial armhf-xenial
-docker build -t mayeut/ljtbuilder2:ppc64el-xenial ppc64el-xenial
-docker build -t mayeut/ljtbuilder2:amd64-centos5 amd64-centos5
+DOCKER_USER=mayeut
+DOCKER_REPO=ljtbuilder2
+
+TAGS="amd64-centos5 arm64-xenial armhf-xenial ppc64el-xenial"
+
+for TAG in ${TAGS}; do
+  docker build -t ${DOCKER_USER}/${DOCKER_REPO}:${TAG} ${TAG}
+done
+
+if [ "${DOCKER_DO_PUSH}" == "1" ] &&  "${DOCKER_DO_LOGIN}" == "1" ; then
+  docker login -u="${DOCKER_USER}" -p="${DOCKER_PASSWORD}"
+fi
+
+if [ "${DOCKER_DO_PUSH}" == "1" ]; then
+  for TAG in ${TAGS}; do
+    docker push ${DOCKER_USER}/${DOCKER_REPO}:${TAG}
+  done
+fi
