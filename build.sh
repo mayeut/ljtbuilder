73a8445886abd39aaa6b65a9d825ce1b6911082f
mayeut <mayeut@users.noreply.github.com>
2016-10-10 22:39:05 +0200

    Update build.sh to use tabs

--- build.sh
+++ build.sh
@@ -18,21 +18,21 @@ for TAG in ${TAGS}; do
 		echo "      services:" >> .travis.yml
 		echo "        - docker" >> .travis.yml
 	else
-  	docker build -t ${DOCKER_USER}/${DOCKER_REPO}:${TAG} ${TAG}
-  	if [ "${DOCKER_DO_PUSH}" == "1" ]; then
-  		docker push ${DOCKER_USER}/${DOCKER_REPO}:${TAG}
-  	fi
-  fi
+		docker build -t ${DOCKER_USER}/${DOCKER_REPO}:${TAG} ${TAG}
+		if [ "${DOCKER_DO_PUSH}" == "1" ]; then
+			docker push ${DOCKER_USER}/${DOCKER_REPO}:${TAG}
+		fi
+	fi
 done
 
 if [ "${GENERATE_TRAVIS}" == "1" ]; then
 	echo "before_install:" >> .travis.yml
 	echo "  - docker run --rm --privileged multiarch/qemu-user-static:register" >> .travis.yml
-  echo "script:" >> .travis.yml
+	echo "script:" >> .travis.yml
 	echo "  - docker build -t ${DOCKER_USER}/${DOCKER_REPO}:\${DOCKER_TAG} \${DOCKER_TAG}" >> .travis.yml
 	echo "after_success:" >> .travis.yml
-  echo "  - if [ \"\${TRAVIS_BRANCH}\" == \"master\" ] && [ \"\${DOCKER_PUSH}\" == \"1\" ]; then" >> .travis.yml
-  echo "    docker login -u=\"${DOCKER_USER}\" -p=\"\${DOCKER_PASSWORD}\";" >> .travis.yml
-  echo "    docker push ${DOCKER_USER}/${DOCKER_REPO}:\${DOCKER_TAG};" >> .travis.yml
-  echo "    fi" >> .travis.yml
+	echo "  - if [ \"\${TRAVIS_BRANCH}\" == \"master\" ] && [ \"\${DOCKER_PUSH}\" == \"1\" ]; then" >> .travis.yml
+	echo "    docker login -u=\"${DOCKER_USER}\" -p=\"\${DOCKER_PASSWORD}\";" >> .travis.yml
+	echo "    docker push ${DOCKER_USER}/${DOCKER_REPO}:\${DOCKER_TAG};" >> .travis.yml
+	echo "    fi" >> .travis.yml
 fi
